edeef5a348618597071fa932bdc6ee28556edf02
Miguel Garcia <miguelangel.garcia@b-reel.com>
2020-02-24 00:47:59 +0100

    add https

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-// import Main from './js/main'
+import Main from './js/main'
 // import Main from './js/mainWagner'
-import Main from './js/mainVR'
+// import Main from './js/mainVR'
 
 // eslint-disable-next-line
 new Main()
